42ae17c09f7a8418ace18371b954676b857e8d6b
Nikhil Devadiga <nmdevadiga216@gmail.com>
2023-08-01 23:13:48 +0530

    Ditched snake case with camel case

--- src/Storage.hs
+++ src/Storage.hs
@@ -4,18 +4,18 @@ module Storage
     , Cursor (..)
     , KeyStore
     , ValueStore
-    , empty_vs
-    , init_vs
-    , append_vs
-    , read_vs
-    , empty_ks
-    , init_ks
-    , insert_ks
-    , lookup_ks
-    , empty_store
-    , init_store
-    , set_store
-    , get_store
+    , emptyVS
+    , initVS
+    , appendVS
+    , readVS
+    , emptyKS
+    , initKS
+    , insertKS
+    , lookupKS
+    , emptyStore
+    , initStore
+    , setStore
+    , getStore
     ) where
 
 import qualified Data.Map as Map
@@ -23,8 +23,8 @@ import qualified Data.ByteString.UTF8 as BS
 
 type Key = BS.ByteString
 type Value = BS.ByteString
-data Cursor = Cursor { get_offset :: Int
-                     , get_length :: Int
+data Cursor = Cursor { getOffset :: Int
+                     , getLength :: Int
                      }
               deriving (Show, Eq)
 type KeyStore = Map.Map Key Cursor
@@ -33,58 +33,58 @@ type Store = (KeyStore, ValueStore)
 
 -- Value Store
 
-empty_vs :: ValueStore
-empty_vs = mempty
+emptyVS :: ValueStore
+emptyVS = mempty
 
-init_vs :: BS.ByteString -> ValueStore
-init_vs value = value
+initVS :: BS.ByteString -> ValueStore
+initVS value = value
 
-append_vs :: Value -> ValueStore -> (ValueStore, Cursor)
-append_vs value vs = (new_vs, cursor)
-    where vs_length = BS.length vs
-          value_length = BS.length value
-          cursor = Cursor { get_offset = vs_length
-                          , get_length = value_length
+appendVS :: Value -> ValueStore -> (ValueStore, Cursor)
+appendVS value vs = (newVS, cursor)
+    where vsLength = BS.length vs
+          valueLength = BS.length value
+          cursor = Cursor { getOffset = vsLength
+                          , getLength = valueLength
                           }
-          new_vs = vs <> value
+          newVS = vs <> value
 
-read_vs :: Cursor -> ValueStore -> (ValueStore, Value)
-read_vs cursor vs = (vs, value)
-    where offset = get_offset cursor
-          len = get_length cursor
+readVS :: Cursor -> ValueStore -> (ValueStore, Value)
+readVS cursor vs = (vs, value)
+    where offset = getOffset cursor
+          len = getLength cursor
           value = BS.take len (BS.drop offset vs)
 
 -- Key Store
 
-empty_ks :: KeyStore
-empty_ks = Map.empty
+emptyKS :: KeyStore
+emptyKS = Map.empty
 
-init_ks :: [(Key, Cursor)] -> KeyStore
-init_ks = Map.fromList
+initKS :: [(Key, Cursor)] -> KeyStore
+initKS = Map.fromList
 
-insert_ks :: Key -> Cursor -> KeyStore -> KeyStore
-insert_ks = Map.insert
+insertKS :: Key -> Cursor -> KeyStore -> KeyStore
+insertKS = Map.insert
 
-lookup_ks :: Key -> KeyStore -> Maybe Cursor
-lookup_ks = Map.lookup
+lookupKS :: Key -> KeyStore -> Maybe Cursor
+lookupKS = Map.lookup
 
 -- Store
 
-empty_store :: Store
-empty_store = (empty_ks, empty_vs)
+emptyStore :: Store
+emptyStore = (emptyKS, emptyVS)
 
-init_store :: [(Key, Cursor)] -> BS.ByteString -> Store
-init_store key_pairs value = (init_ks key_pairs, init_vs value)
+initStore :: [(Key, Cursor)] -> BS.ByteString -> Store
+initStore keyPairs value = (initKS keyPairs, initVS value)
 
-set_store :: Key -> Value -> Store -> Store
-set_store key value (ks, vs) = new_store
-    where (new_vs, cursor) = append_vs value vs
-          new_ks = insert_ks key cursor ks
-          new_store = (new_ks, new_vs)
+setStore :: Key -> Value -> Store -> Store
+setStore key value (ks, vs) = newStore
+    where (newVS, cursor) = appendVS value vs
+          newKS = insertKS key cursor ks
+          newStore = (newKS, newVS)
 
-get_store :: Key -> Store -> (Store, Maybe Value)
-get_store key store@(ks, vs) = let maybe_cursor = lookup_ks key ks
-                                   maybe_value = case maybe_cursor of
+getStore :: Key -> Store -> (Store, Maybe Value)
+getStore key store@(ks, vs) = let maybeCursor = lookupKS key ks
+                                  maybeValue = case maybeCursor of
                                                     Nothing -> Nothing
-                                                    Just cursor -> Just . snd . read_vs cursor $ vs
-                               in  (store, maybe_value)
+                                                    Just cursor -> Just . snd . readVS cursor $ vs
+                               in (store, maybeValue)
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,32 +8,33 @@ main = runTestTT testSuite
 testSuite :: Test
 testSuite = TestList [ testValueStore
                      , testKeyStore
+                     , testStore
                      ]
 
 testValueStore :: Test
 testValueStore = TestList [ TestCase (assertEqual "testValueStore - Append value store with given value"
-                                        (S.append_vs (BS.fromString " world") (S.init_vs (BS.fromString "hello")))
-                                        ((S.init_vs (BS.fromString "hello world"), S.Cursor 5 6)))
+                                        (S.appendVS (BS.fromString " world") (S.initVS (BS.fromString "hello")))
+                                        ((S.initVS (BS.fromString "hello world"), S.Cursor 5 6)))
                           , TestCase (assertEqual "testValueStore - Read value store with given cursor"
-                                        (S.read_vs (S.Cursor 6 6) (S.init_vs (BS.fromString "hello world")))
-                                        ((S.init_vs (BS.fromString "hello world"), (BS.fromString "world"))))
+                                        (S.readVS (S.Cursor 6 6) (S.initVS (BS.fromString "hello world")))
+                                        ((S.initVS (BS.fromString "hello world"), (BS.fromString "world"))))
                           ]
                           
 testKeyStore :: Test
 testKeyStore = TestList [ TestCase (assertEqual "testKeyStore - Insert key into the key store"
-                                      (S.insert_ks (BS.fromString "pizza") (S.Cursor 5 6) (S.empty_ks))
-                                      ((S.init_ks [((BS.fromString "pizza"), S.Cursor 5 6)])))
+                                      (S.insertKS (BS.fromString "pizza") (S.Cursor 5 6) (S.emptyKS))
+                                      ((S.initKS [((BS.fromString "pizza"), S.Cursor 5 6)])))
                         , TestCase (assertEqual "testKeyStore - Read cursor store from key store"
-                                      (S.lookup_ks (BS.fromString "pizza") (S.init_ks [((BS.fromString "pizza"), S.Cursor 5 6)]))
+                                      (S.lookupKS (BS.fromString "pizza") (S.initKS [((BS.fromString "pizza"), S.Cursor 5 6)]))
                                       (Just (S.Cursor 5 6)))
                         ]
 
 testStore :: Test
 testStore = let key = BS.fromString "1"
                 value = BS.fromString "One"
-                store_0 = S.empty_store
-                store_1 = S.set_store key value store_0
-                (store_2, maybe_value) = S.get_store key store_1
+                store0 = S.emptyStore
+                store1 = S.setStore key value store0
+                (store2, maybeValue) = S.getStore key store1
             in  TestList [ TestCase (assertEqual "testStore - Setting and the getting the same value from the store"
                                         (Just value)
-                                        maybe_value)]
+                                        maybeValue)]
